96b3597dd520d908e1071a20fb697bb173ecb0d2
Jordan Coff <jordan.a.coff@gmail.com>
2016-11-09 13:05:41 -0800

    Initial Commit

---
+++ JCAnnotationDrawer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCAnnotationDrawer
+//
+//  Created by Jordan Coff on 11/9/16.
+//  Copyright © 2016 JordanCoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCAnnotationDrawer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCAnnotationDrawer
+//
+//  Created by Jordan Coff on 11/9/16.
+//  Copyright © 2016 JordanCoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
